97743f33abd468f049e57400abc385000b61a8d9
Chips <andriistefanenko+github@gmail.com>
2025-03-06 23:23:06 +0200

    Added Description component and fixed project structure

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,11 @@
+import styles from './Description.module.css';
+
+const Description = () => {
+  return (
+    <p className={styles.description}>
+      Please leave your feedback about our service by selecting one of the options below.
+    </p>
+  );
+};
+
+export default Description;
